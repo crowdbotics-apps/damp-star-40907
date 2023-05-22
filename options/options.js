2d7db4f6ae1860530371908c0ce3b6c25b34fc52
Crowdbotics <team@crowdbotics.com>
2023-05-22 23:59:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-star-40907",
+  url: "https://damp-star-40907.botics.co",
+  api: "https://damp-star-40907.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
